49059c0edf4d18465a956c352e2016a360a9dddc
Chetan-zluri <chethan.m@zluri.com>
2025-01-23 15:35:09 +0530

    final 2

--- src/server.ts
+++ src/server.ts
@@ -12,18 +12,18 @@ const main = async (): Promise<Express> => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use("/api/transactions", transactionRoutes);
-  const upload = multer({
-    dest: "uploads/", // Temporary folder to store uploaded files
-    limits: { fileSize: 1 * 1024 * 1024 }, // 1 MB size limit
-    fileFilter: (req, file, cb) => {
-      const ext = path.extname(file.originalname);
-      if (ext !== ".csv") {
-        return cb(new Error("Only CSV files are allowed"));
-      }
-      cb(null, true);
-    },
-  });
-  app.use(upload.single("file"));
+  // const upload = multer({
+  //   dest: "uploads/", // Temporary folder to store uploaded files
+  //   limits: { fileSize: 1 * 1024 * 1024 }, // 1 MB size limit
+  //   fileFilter: (req, file, cb) => {
+  //     const ext = path.extname(file.originalname);
+  //     if (ext !== ".csv") {
+  //       return cb(new Error("Only CSV files are allowed"));
+  //     }
+  //     cb(null, true);
+  //   },
+  // });
+  // app.use(upload.single("file"));
   app.post(
     "/api/upload",
     async (req: Request, res: Response, next: NextFunction) => {
