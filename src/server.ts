f2f3021839aa0e7f9a78b31abb4832181c29ec37
Chetan-zluri <chethan.m@zluri.com>
2025-01-23 15:26:46 +0530

    final stage

--- src/server.ts
+++ src/server.ts
@@ -7,39 +7,39 @@ import transactionRoutes from "./routes/transaction.routes";
 import { uploadCSVController } from "./controllers/transaction.controller";
 const main = async (): Promise<Express> => {
   const app = express();
-  // app.use(express.json());
-  // app.use(cors());
-  // app.use(bodyParser.json());
-  // app.use(bodyParser.urlencoded({ extended: true }));
-  // app.use("/api/transactions", transactionRoutes);
-  // const upload = multer({
-  //   dest: "uploads/", // Temporary folder to store uploaded files
-  //   limits: { fileSize: 1 * 1024 * 1024 }, // 1 MB size limit
-  //   fileFilter: (req, file, cb) => {
-  //     const ext = path.extname(file.originalname);
-  //     if (ext !== ".csv") {
-  //       return cb(new Error("Only CSV files are allowed"));
-  //     }
-  //     cb(null, true);
-  //   },
-  // });
-  // app.use(upload.single("file"));
-  // app.post(
-  //   "/api/upload",
-  //   async (req: Request, res: Response, next: NextFunction) => {
-  //     try {
-  //       await uploadCSVController(req, res, next);
-  //     } catch (error) {
-  //       next(error);
-  //     }
-  //   }
-  // );
-  // app.get("/", (_, res) => {
-  //   res.status(200).json({ message: "Server is running!" });
-  // });
-  // app.get("/error", () => {
-  //   throw new Error("Test internal server error");
-  // });
+  app.use(express.json());
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use("/api/transactions", transactionRoutes);
+  const upload = multer({
+    dest: "uploads/", // Temporary folder to store uploaded files
+    limits: { fileSize: 1 * 1024 * 1024 }, // 1 MB size limit
+    fileFilter: (req, file, cb) => {
+      const ext = path.extname(file.originalname);
+      if (ext !== ".csv") {
+        return cb(new Error("Only CSV files are allowed"));
+      }
+      cb(null, true);
+    },
+  });
+  app.use(upload.single("file"));
+  app.post(
+    "/api/upload",
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        await uploadCSVController(req, res, next);
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
+  app.get("/", (_, res) => {
+    res.status(200).json({ message: "Server is running!" });
+  });
+  app.get("/error", () => {
+    throw new Error("Test internal server error");
+  });
   app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     console.error(err.stack);
     res.status(500).json({ message: `Internal server error: ${err.message}` });
